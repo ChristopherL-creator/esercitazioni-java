84b8e8526d5bd14ec19957dbeb6bbdfd0df69b0a
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-06 10:47:59 +0200

    Update script.js

---
+++ script.js
@@ -0,0 +1,70 @@
+const arrayOfNumbers = [23, 13, 34, 56, 99, 101]; 
+
+//  cercare numero 56 e stamparlo (console);
+
+function print56(arrayOfNumbers) { 
+    const newArray1 = []; 
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        const number = arrayOfNumbers[i];
+        if (number === 56) {
+            newArray1.push(number);
+        } 
+    } 
+
+    return newArray1;
+} 
+
+console.log('cercare numero 56: ', print56(arrayOfNumbers)); 
+
+//  stampare solo valori successivi a 56; 
+
+function from56(arrayOfNumbers) { 
+    const newArray2 = []; 
+
+  for (let i = 0; i < arrayOfNumbers.length; i++) {
+      const number = arrayOfNumbers[i]; 
+      if (number > 56) {
+          newArray2.push(number);
+      }
+
+    } 
+
+    return newArray2;
+} 
+
+console.log('valori successivi a 56: ', from56(arrayOfNumbers)); 
+
+//  stampare tutti valori tranne 56; 
+
+function printAllBut56(arrayOfNumbers) {
+    const newArray3 = []; 
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        const number = arrayOfNumbers[i]; 
+                if (number != 56) {
+                    newArray3.push(number);
+                } 
+    } 
+
+    return newArray3;
+} 
+
+console.log('tutti valori tranne 56: ', printAllBut56(arrayOfNumbers)); 
+
+//  stampare solo numeri dispari; 
+
+function printOdd(arrayOfNumbers) {
+    const newArray = []; 
+
+    for (let i = 0; i < arrayOfNumbers.length; i++) {
+        const number = arrayOfNumbers[i]; 
+                if (number % 2 === 1) {
+                    newArray.push(number);
+                } 
+    } 
+
+    return newArray;
+} 
+
+console.log('numeri dispari: ', printOdd(arrayOfNumbers)); 
