4e7125e59e19a54958f10014460f6ecc1e4e5df0
ChristopherL <101825137+ChristopherL-creator@users.noreply.github.com>
2022-05-06 13:29:29 +0200

    ww

--- script.js
+++ script.js
@@ -1,80 +1,81 @@
-const arrayOfNumbers = [23, 13, 34, 56, 99, 101]; 
+// const arrayOfNumbers = [23, 13, 34, 56, 99, 101]; 
 
-//  cercare numero 56 e stamparlo (console);
+// //  cercare numero 56 e stamparlo (console);
 
-function print56(arrayOfNumbers) { 
-    const newArray1 = []; 
+// function print56(arrayOfNumbers) { 
+//     const newArray1 = []; 
 
-    for (let i = 0; i < arrayOfNumbers.length; i++) {
-        const number = arrayOfNumbers[i];
-        if (number === 56) {
-            newArray1.push(number); 
-            break; 
-        } 
-    } 
-
-    return newArray1;
-} 
-
-console.log('cercare numero 56: ', print56(arrayOfNumbers)); 
-
-//  stampare solo valori successivi a 56; 
-
-function from56(arrayOfNumbers) { 
-    const newArray2 = []; 
-
-  for (let i = 0; i < arrayOfNumbers.length; i++) {
-      const number = arrayOfNumbers[i]; 
-      if (i > 3) {
-          newArray2.push(number);
-      }
-
-    } 
-
-    return newArray2;
-} 
-
-console.log('valori successivi a 56: ', from56(arrayOfNumbers)); 
-
-//  stampare tutti valori tranne 56; 
-
-// for (let i = 0; i < array.length; i++) {
-//     if (array[i] == 56) {
-//         continue;
+//     for (let i = 0; i < arrayOfNumbers.length; i++) {
+//         const number = arrayOfNumbers[i];
+//         if (number === 56) {
+//             newArray1.push(number); 
+//             break; 
+//         } 
 //     } 
 
-// }
+//     return newArray1;
+// } 
 
-// console.log('tutti valori tranne 56: ', arrayOfNumbers);  
+// console.log('cercare numero 56: ', print56(arrayOfNumbers)); 
 
-function printAllBut56(arrayOfNumbers) {
-    const newArray3 = []; 
+// //  stampare solo valori successivi a 56; 
 
-    for (let i = 0; i < arrayOfNumbers.length; i++) {
-        const number = arrayOfNumbers[i]; 
-                if (number != 56) {
-                    newArray3.push(number);
-                } 
-    } 
+// function from56(arrayOfNumbers) { 
+//     const newArray2 = []; 
 
-    return newArray3;
-} 
+//   for (let i = 0; i < arrayOfNumbers.length; i++) {
+//       const number = arrayOfNumbers[i]; 
+//       if (i > 3) {
+//           newArray2.push(number);
+//       }
 
-console.log('tutti valori tranne 56: ', printAllBut56(arrayOfNumbers)); 
+//     } 
 
-//  stampare solo numeri dispari; 
+//     return newArray2;
+// } 
 
-function printOdd(arrayOfNumbers) {
-    const newArray = []; 
+// console.log('valori successivi a 56: ', from56(arrayOfNumbers)); 
 
-    for (let i = 0; i < arrayOfNumbers.length; i++) {
-        const number = arrayOfNumbers[i]; 
-                if (number % 2 === 1) {
-                    newArray.push(number);
-                } 
-    } 
+// //  stampare tutti valori tranne 56; 
 
-    return newArray;
-} 
+// // for (let i = 0; i < array.length; i++) {
+// //     if (array[i] == 56) {
+// //         continue;
+// //     } 
 
-console.log('numeri dispari: ', printOdd(arrayOfNumbers)); 
+// // }
+
+// // console.log('tutti valori tranne 56: ', arrayOfNumbers);  
+
+// function printAllBut56(arrayOfNumbers) {
+//     const newArray3 = []; 
+
+//     for (let i = 0; i < arrayOfNumbers.length; i++) {
+//         const number = arrayOfNumbers[i]; 
+//                 if (number != 56) {
+//                     newArray3.push(number);
+//                 } 
+//     } 
+
+//     return newArray3;
+// } 
+
+// console.log('tutti valori tranne 56: ', printAllBut56(arrayOfNumbers)); 
+
+// //  stampare solo numeri dispari; 
+
+// function printOdd(arrayOfNumbers) {
+//     const newArray = []; 
+
+//     for (let i = 0; i < arrayOfNumbers.length; i++) {
+//         const number = arrayOfNumbers[i]; 
+//                 if (number % 2 === 1) {
+//                     newArray.push(number);
+//                 } 
+//     } 
+
+//     return newArray;
+// } 
+
+// console.log('numeri dispari: ', printOdd(arrayOfNumbers)); 
+
